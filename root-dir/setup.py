ed3c99d18bd603983181b3f3559e5480f485328c
Ribamar Santarosa <ribamar.santarosa@brightcomputing.com>
2017-09-14 17:29:59 +0200

    root-dir: setup.py filled

---
+++ root-dir/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = "pychampselysees",
+  packages = ["pychampselysees"],
+  version = "0.1",
+  description = "many python functions for general purpose",
+  author = "Ribamar Santarosa",
+  author_email = "ribamar@gmail.com",
+  url = "https://github.com/ribamar-santarosa/pychampselysees",
+  download_url = "https://github.com/ribamar-santarosa/pychampselysees/archive/0.1.tar.gz",
+  keywords = ["testing", "example"],
+  classifiers = [],
+)
+
